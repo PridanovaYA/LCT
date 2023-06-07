613a3da1ba1207d79dbc980b04504ec69ebcd58b
PridanovaYA <pridanova.01@yandex.ru>
2023-06-08 03:26:29 +0400

    update

--- main.py
+++ main.py
@@ -13,11 +13,8 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
                              [0.59590059, -0.27455667, -0.32134392],
                              [0.21153661, -0.52273617, 0.31119955]])
     OrigShape = imgRGB.shape
-    tmp = imgRGB.reshape(-1, 3)
     Matrix_YIQ = np.dot(imgRGB.reshape(-1, 3), yiq_from_rgb.transpose()).reshape(OrigShape)
     return Matrix_YIQ
-
-    pass
 
 
 def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
@@ -26,8 +23,6 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
                              [0.21153661, -0.52273617, 0.31119955]])
     OrigShape = imgYIQ.shape
     return np.dot(imgYIQ.reshape(-1, 3), np.linalg.inv(yiq_from_rgb).transpose()).reshape(OrigShape)
-
-    pass
 
 
 def getting_matrix_Tk(Teta_k):
@@ -40,57 +35,59 @@ def getting_matrix_Tk(Teta_k):
 if __name__ == "__main__":
     img = imread('test3.jpg')
 
-    print()
-    # r1, g1, b1 = img.split()
-    # print(img[0][0])
-    # print(img[0][0].transpose())
-
+    """отрисовка исходника"""
     fig = plt.figure(figsize=(20, 10))
     fig.add_subplot(2, 3, 1)
     title('Исходное изображение(rgb)')
-    # в цветовом пространстве opencv изображения загружаются в цветовом пространстве BGR
+    """в цветовом пространстве opencv изображения загружаются в цветовом пространстве BGR"""
     plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    k = 60
 
+    """реализация первого шага алгоритма"""
     T_YIQ = transformRGB2YIQ(img)
-
-    T_YIQ_inv = pinv(T_YIQ)
-
-    Teta_k = (math.pi * k) / 180
-
-    T_k = getting_matrix_Tk(Teta_k)
-
-    T_YIQ_ = T_YIQ.reshape(-1, 3)
-    T_YIQ_inv_ = T_YIQ_inv.reshape(-1, 3)
-
-    g_k = np.dot(T_YIQ_, T_k, T_YIQ_inv_)
-    print(g_k)
-
     fig.add_subplot(2, 3, 2)
     title('После преобразования цвета(yiq)')
     imshow(T_YIQ)
-    # show()
-    img_ = img.reshape(-1, 3)
-    # imshow(img_)
 
-    n, q = np.shape(img_)
+    """реализация второго шага алгоритма"""
+    k = 60
+    Teta_k = (math.pi * k) / 180
+
+    """реализация третьего шага алгоритма"""
+    T_YIQ_inv = pinv(T_YIQ)
+    T_k = getting_matrix_Tk(Teta_k)
+    T_YIQ_ = T_YIQ.reshape(-1, 3)
+    T_YIQ_inv_ = T_YIQ_inv.reshape(-1, 3)
+    g_k = np.dot(T_YIQ_, T_k, T_YIQ_inv_)
+    # print(g_k)
+
+    """проверка развертывания изображния"""
+    fig.add_subplot(2, 3, 3)
+    img_ = img.reshape(-1, 3)
+    img_v = img_.reshape(img.shape)
+    imshow(img_v)
+
+    """попытка вычленить массивы, отвечающие за цвет"""
     # p = np.zeros((m, n, q))
-    # p = img.reshape(1,3)
-    # k=0
-    for i in range(0, n):
-            img_[n][0] = img_[n][0].transpose()
-    #         k+=1
+    # p = img.reshape(1, 3)
+    # k = 0
+    # for i in range(0, n):
+    #     img_[n][0] = img_[n][0].transpose()
+    #     k += 1
     #
     # print(p)
-
-    im1 = Image.open('test3.jpg')
-    imag = im1.convert('RGB')
-    X, Y = 0, 0
-    pixelRGB = imag.getpixel((X, Y))
-    R, G, B = pixelRGB
+    # n, q = np.shape(img_)
+    # im1 = Image.open('test3.jpg')
+    # imag = im1.convert('RGB')
+    # X, Y = 0, 0
+    # pixelRGB = imag.getpixel((X, Y))
+    # R, G, B = pixelRGB
     # r1, g1, b1 = im1.split()
-    print(R, G, B)
+    # print(R, G, B)
+
+    ## еще один способ перехода из rgb -> yiq
     # I = imread('test.jpg');
     # imshow(I);
     # J = rgb2ntsc(I);
     # imshow(J);
+
+    show()
